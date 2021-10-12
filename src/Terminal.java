93f9f87947bb1ab6614046eb943daf63a515b262
RedCupCrowe <valdemarkjohansen@gmail.com>
2021-10-12 10:51:34 +0200

    Deleted terminal, added Menukort

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,10 +1,1 @@
-//import java.util.Sorted
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-public class Terminal {
-  public static void main(String[] args) {
-    System.out.println(LocalTime.now());
-  }
-}
