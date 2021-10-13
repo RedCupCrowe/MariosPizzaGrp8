ede75066788615ba8466b659c5dfc171ec555f62
RedCupCrowe <valdemarkjohansen@gmail.com>
2021-10-13 12:30:16 +0200

    PizzaRestaurant

---
+++ src/PizzaRestaurant.java
@@ -0,0 +1,10 @@
+public class PizzaRestaurant {
+  /*
+  Skal kunne kalde menukortet
+  skal kunne starte en bestilling
+  skal kunne anullere en bestilling imens og efter den er lavet
+  skal kunne afslutte en bestilling
+  skal kunne gemme bestillingen efter afslutning
+  skal evt. kunne kalde statistik.
+  */
+}
--- src/Storage.java
+++ src/Storage.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+
 public class Storage {
+ArrayList<Pizza> pizzaBestillinger = new ArrayList<>();
+
 
 }
